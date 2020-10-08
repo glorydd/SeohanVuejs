5d044b7e54dfb61ab496584a1ca0456bee1892bd
Min Mike <minssan9@gmail.com>
2020-10-08 16:49:31 +0900

    webpack.config file 정리 #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
+const config = require('./config')
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
+// function resolve (dir) {
+//   return path.join(__dirname, '..', dir)
+// }
 
 module.exports = {
   entry: './src/main.js',
@@ -52,42 +55,27 @@ module.exports = {
   },
   devServer: {
     port:8091,
-    historyApiFallback: {
-      rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-      ],
-    },
+    contentBase: './src',
+    historyApiFallback: true,
     noInfo: true,
     disableHostCheck: true,
-    clientLogLevel: 'warning',
-    hot: true,
-    contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
-    overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
-      : false,
+    open: config.dev.autoOpenBrowser,
+    publicPath: config.dev.assetsPublicPath,
     quiet: true, // necessary for FriendlyErrorsPlugin
-    watchOptions: {
-      poll: config.dev.poll,
-    },
-
   },
   performance: {
     hints: false
   },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      'window.jQuery': 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
